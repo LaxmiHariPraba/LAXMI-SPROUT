7142cf4e476c362f30eadf8c1cb063a7cab0ddb9
Laxmi HariPraba B V <79656342+LaxmiHariPraba@users.noreply.github.com>
2021-06-20 18:22:35 +0530

    Add files via upload

---
+++ Laxmi Sprout 18.py
@@ -0,0 +1,154 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/LAXMI HARIPRABA B V/AppData/Local/Programs/Python/Python39/laxmisprout.py
+Traceback (most recent call last):
+  File "C:/Users/LAXMI HARIPRABA B V/AppData/Local/Programs/Python/Python39/laxmisprout.py", line 1, in <module>
+    import pymongo
+ModuleNotFoundError: No module named 'pymongo'
+>>> 
+= RESTART: C:/Users/LAXMI HARIPRABA B V/AppData/Local/Programs/Python/Python39/laxmisprout.py
+
+SHOPPING 
+1.SHOW LIST(DISPLAY ALL THE ITEMS)
+2.CREATE(ADDING ITEM)
+3.SEE THE COST(READ THE COLLECIONS)
+4.UPDATE(CHANGE THE COST OF  ITEM)
+5.DELETE(REMOVE FROM COLLECTION)
+6.BUY ITEMS
+7.EXIT
+
+Enter your choice : 1
+
+ITEM LIST
+
+Press enter to continue
+
+
+SHOPPING 
+1.SHOW LIST(DISPLAY ALL THE ITEMS)
+2.CREATE(ADDING ITEM)
+3.SEE THE COST(READ THE COLLECIONS)
+4.UPDATE(CHANGE THE COST OF  ITEM)
+5.DELETE(REMOVE FROM COLLECTION)
+6.BUY ITEMS
+7.EXIT
+
+Enter your choice : 2
+Enter the name of item : Xpect A
+Enter the cost of the item : 20
+ADDING NEW ITEM TO COLLECTION
+
+Xpect A added to the collection with cost  20 Rs
+
+Press enter to continue
+
+
+SHOPPING 
+1.SHOW LIST(DISPLAY ALL THE ITEMS)
+2.CREATE(ADDING ITEM)
+3.SEE THE COST(READ THE COLLECIONS)
+4.UPDATE(CHANGE THE COST OF  ITEM)
+5.DELETE(REMOVE FROM COLLECTION)
+6.BUY ITEMS
+7.EXIT
+
+Enter your choice : 2
+Enter the name of item : Xpect B
+Enter the cost of the item : 35
+ADDING NEW ITEM TO COLLECTION
+
+Xpect B added to the collection with cost  35 Rs
+
+Press enter to continue
+1
+
+SHOPPING 
+1.SHOW LIST(DISPLAY ALL THE ITEMS)
+2.CREATE(ADDING ITEM)
+3.SEE THE COST(READ THE COLLECIONS)
+4.UPDATE(CHANGE THE COST OF  ITEM)
+5.DELETE(REMOVE FROM COLLECTION)
+6.BUY ITEMS
+7.EXIT
+
+Enter your choice : 1
+
+ITEM LIST
+1 : Xpect A
+2 : Xpect B
+
+Press enter to continue
+
+= RESTART: C:/Users/LAXMI HARIPRABA B V/AppData/Local/Programs/Python/Python39/laxmisprout.py
+
+SHOPPING 
+1.SHOW LIST(DISPLAY ALL THE ITEMS)
+2.CREATE(ADDING ITEM)
+3.SEE THE COST(READ THE COLLECIONS)
+4.UPDATE(CHANGE THE COST OF  ITEM)
+5.DELETE(REMOVE FROM COLLECTION)
+6.BUY ITEMS
+7.EXIT
+
+Enter your choice : 1
+
+ITEM LIST
+1 : math
+
+Press enter to continue
+
+
+SHOPPING 
+1.SHOW LIST(DISPLAY ALL THE ITEMS)
+2.CREATE(ADDING ITEM)
+3.SEE THE COST(READ THE COLLECIONS)
+4.UPDATE(CHANGE THE COST OF  ITEM)
+5.DELETE(REMOVE FROM COLLECTION)
+6.BUY ITEMS
+7.EXIT
+
+Enter your choice : 2
+Enter the name of item : DOLO 650
+Enter the cost of the item : 17
+ADDING NEW ITEM TO COLLECTION
+
+DOLO 650 added to the collection with cost  17 Rs
+
+Press enter to continue
+
+
+SHOPPING 
+1.SHOW LIST(DISPLAY ALL THE ITEMS)
+2.CREATE(ADDING ITEM)
+3.SEE THE COST(READ THE COLLECIONS)
+4.UPDATE(CHANGE THE COST OF  ITEM)
+5.DELETE(REMOVE FROM COLLECTION)
+6.BUY ITEMS
+7.EXIT
+
+Enter your choice : ASCORIL D
+Traceback (most recent call last):
+  File "C:/Users/LAXMI HARIPRABA B V/AppData/Local/Programs/Python/Python39/laxmisprout.py", line 77, in <module>
+    ch = int(input("Enter your choice : "))
+ValueError: invalid literal for int() with base 10: 'ASCORIL D'
+>>> 
+= RESTART: C:/Users/LAXMI HARIPRABA B V/AppData/Local/Programs/Python/Python39/laxmisprout.py
+
+SHOPPING 
+1.SHOW LIST(DISPLAY ALL THE ITEMS)
+2.CREATE(ADDING ITEM)
+3.SEE THE COST(READ THE COLLECIONS)
+4.UPDATE(CHANGE THE COST OF  ITEM)
+5.DELETE(REMOVE FROM COLLECTION)
+6.BUY ITEMS
+7.EXIT
+
+Enter your choice : 2
+Enter the name of item : ASCORIL D
+Enter the cost of the item : 45
+ADDING NEW ITEM TO COLLECTION
+
+ASCORIL D added to the collection with cost  45 Rs
+
+Press enter to continue
